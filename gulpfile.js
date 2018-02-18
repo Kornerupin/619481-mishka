5299da682d16bd01730e6901c6d480de22bf4f08
Kornerupin <kornerupin@yandex.ru>
2018-02-18 19:07:46 +0300

    Сделал автоматизацию сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 "use strict";
 
 var gulp = require("gulp");
-var sass = require("gulp-sass");
+var del = require("del");
 var rename = require("gulp-rename");
-var svgstore = require("gulp-svgstore");
 var plumber = require("gulp-plumber");
+var run = require("run-sequence");
+var server = require("browser-sync").create();
+
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+
+var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+
+
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -16,13 +28,16 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -30,7 +45,17 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]);
+});
+
+gulp.task("images", function () {
+  return gulp.src("souce/img/**/*.{png,jpg,jpeg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("souce/img"));
 });
 
 gulp.task("sprite", function () {
@@ -39,5 +64,44 @@ gulp.task("sprite", function () {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg,jpeg}")
+    .pipe(webp({quality: 80}))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "sprite",
+    "html",
+    done);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/gonts/**/*.{woff, woff2}",
+      "source/img/**",
+      "source/js/**"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
 });
